ff376122f3effdb06230c47bc5cd80fbcbb45aeb
Alicia Pizarro <43381268+APizarro8@users.noreply.github.com>
2019-04-25 15:43:03 +0200

    Update OGR.py

--- OGR.py
+++ OGR.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #Ejercicio 1 parte I
-#Propósito: Utilizar métodos de GDAL/OGR para consultar información vectorial, modificarla o eliminarla.
+#Propósito: Utilizar métodos de OGR para consultar información vectorial, modificarla o eliminarla.
 #Autor: Alicia Pizarro
 #Fecha: 30/12/2017
 
@@ -8,7 +8,7 @@ import ogr
 import os
 
 #Establecer el espacio de trabajo
-path = 'D:\\Practica4_py\\UNEP-EDE__forest_area__1445948601'
+path = 'xxxDATAxxx'
 os.chdir(path)
 
 #Obtener el driver para shapefile
@@ -21,7 +21,7 @@ else:
     print  'El driver {0} es correcto.'.format(driverName)
     
 #Abrir el shapefile de origen para lectura: el 0 para lectura, y el 1 para modificar
-shapefile = 'UNEP-EDE__forest_area__1445948601.shp'
+shapefile = 'xxDATAxxx.shp'
 dataSource = driver.Open(shapefile, 1)
 
 #Obtener el número de layers del shapefile
@@ -73,7 +73,6 @@ for feature in layer:
     print feature.GetField("NAME")
 print '\n'
 
-
 #Modificar el valor del campo DEVELOPED de la región con ID = 0 asignándole el valor 1
 feature = layer.GetFeature(0) #Posicionarse 
 feature.SetField("DEVELOPED", 1) #Establecer nuevo valor
@@ -89,6 +88,8 @@ for feature in layer:
 layer.SetAttributeFilter(None) #Limpiar filtro
 layer.GetFeature(0) #Reinicializar lectura
 
-
 feature.Destroy()
 dataSource.Destroy()
+
+################################################ FINISH ####################################################
+############################################################################################################
